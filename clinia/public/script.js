ab7998112168743e312f9f7f0307c5d3a19f30ac
RokuMembred <fidel_34luk@hotmail.com>
2025-01-16 02:38:11 -0600

    Update script.js

--- clinia/public/script.js
+++ clinia/public/script.js
@@ -1,27 +1,3 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // Obtener referencia a los botones
-  const saveButton = document.getElementById("saveButton");
-  const printButton = document.getElementById("printButton");
-
-  // Función para guardar como PDF
-  saveButton.addEventListener("click", () => {
-    const elementToPrint = document.querySelector(".container"); // Selecciona el contenedor
-    const opt = {
-      margin: 1,
-      filename: `Historia_Clinica_${new Date().getTime()}.pdf`,
-      image: { type: "jpeg", quality: 0.98 },
-      html2canvas: { scale: 2 },
-      jsPDF: { unit: "pt", format: "a4", orientation: "portrait" },
-    };
-    // Genera el PDF usando html2pdf
-    html2pdf().set(opt).from(elementToPrint).save();
-  });
-  // Función para imprimir
-  printButton.addEventListener("click", () => {
-    window.print();
-  });
-});
-
 /*-------*/
 @media print {
   #guardarBtn,
